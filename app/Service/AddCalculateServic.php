dc050c88482047e801190622f7af8b4ba5403984
AlaaShalabe <93447146+AlaaShalabe@users.noreply.github.com>
2023-03-10 22:53:36 +0200

    implements the calculator interface

---
+++ app/Service/AddCalculateServic.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Servic;
+
+use App\Contacts\CalculateServiceContract;
+
+class AddCalculateServic implements CalculateServiceContract
+{
+    private $sing =  '+';
+
+    public function calculat($val1, $val2)
+    {
+
+        return $val1 + $val2;
+    }
+    public function getSign()
+    {
+        return $this->sing;
+    }
+}
